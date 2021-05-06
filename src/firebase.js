a9bbed8414c89e2b1af5b4ea2730275535018847
Payal Agrawal <agrawalpayal1408@gmail.com>
2021-05-06 17:02:20 +0530

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA1k0DNKy9PTNAmf_KYR4mCV_O-UKfRtOQ",
+    authDomain: "whatsapp-clone-dfc4d.firebaseapp.com",
+    projectId: "whatsapp-clone-dfc4d",
+    storageBucket: "whatsapp-clone-dfc4d.appspot.com",
+    messagingSenderId: "544826665295",
+    appId: "1:544826665295:web:8496f12f409ffdee5f3d3a",
+    measurementId: "G-46SE6KD8WJ"
+  };
